d26dae852027e0ebb9c1825a6b0c9da62e0216a6
andreialex232 <numauitnuintelegi@gmail.com>
2024-06-23 23:10:40 +0300

    guest

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
 let dbConnection;
-const uri = "mongodb+srv://alexx:IwrAyYKhfVfp562p@cluster0.j0wq37j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uriGuest = "mongodb+srv://guestguest:guestguest@cluster0.j0wq37j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect(uri)
+    MongoClient.connect(uriGuest)
       .then((client) => {
         dbConnection = client.db("Allaord");
         return cb();
